fd8eaf36f4298c2036ba60943558919f52d45547
Chiwi <stanmma@gmail.com>
2022-11-21 10:20:33 +1000

    add filter script

--- cypress/e2e/filters.cy.ts
+++ cypress/e2e/filters.cy.ts
@@ -1,7 +1,34 @@
+import { RACING_CATEGORIES } from "../config/constants";
+
 describe('Category Filters', () => {
-  it.skip('Insert test here');
+  beforeEach(() => {
+    cy.visit('/');
+  });
+  // Testing failed because defect against spec
+  it('Should uncheck successfully for all categoriesy', () => {
+    RACING_CATEGORIES.forEach((category) => {
+      cy.get(`[data-testid=category-filter-${category.categoryId}]`).within(() => {
+        cy.get('[data-testid=category-filter-checkbox]').uncheck().should('not.be.checked');
+      });
+    })
+  });
 
-  it.skip('Insert test here');
+  it('Should check successfully for all categories', () => {
+    RACING_CATEGORIES.forEach((category) => {
+      cy.get(`[data-testid=category-filter-${category.categoryId}]`).within(() => {
+        cy.get('[data-testid=category-filter-checkbox]').check().should('be.checked');
+      });
+    })
+  });
 
-  it.skip('Insert test here');
-});
+  // Testing failed because defect against spec
+  it('Should empty the list when no filter applied', () => {
+    RACING_CATEGORIES.forEach((category) => {
+      cy.get(`[data-testid=category-filter-${category.categoryId}]`).within(() => {
+        cy.get('[data-testid=category-filter-checkbox]').uncheck();
+      });
+    })
+    cy.get('[data-testid=category-filters]').next().should('be.empty')
+  });
+
+})
